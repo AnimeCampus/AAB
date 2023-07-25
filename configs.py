bb0f345f3e67ab54a87d7bf0b6337ab2b18e6941
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-07-25 23:38:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6655586588:AAGVpTOBW2IflwbS0WOEhAmXfyrUn-DZKVs")
+    FSUB = getenv("FSUB", "Anime_Campus")
     CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    SUDO = list(map(int, getenv("SUDO", "6198858059, ").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
