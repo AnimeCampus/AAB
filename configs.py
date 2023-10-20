c27f649a8d3a9275c9133f6023a1f9b9e2383d0c
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-20 16:39:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "19099900"))
     API_HASH = getenv("API_HASH", "2b445de78e5baf012a0793e60bd4fbf5")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6655586588:AAGVpTOBW2IflwbS0WOEhAmXfyrUn-DZKVs")
-    FSUB = getenv("FSUB", "Anime_Campus")
-    CHID = int(getenv("CHID", "-1001453145062"))
-    SUDO = list(map(int, getenv("SUDO", "6265459491 6198858059").split()))
+    BOT_TOKEN = getenv("BOT_TOKEN", "6778454660:AAHIJCHf4_i4J_AUdpPUBNktGeOIG80epA8")
+    FSUB = getenv("FSUB", "illuminatiXNetwork")
+    CHID = int(getenv("CHID", "-1002115636462"))
+    SUDO = list(map(int, getenv("SUDO", "6299128233").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
