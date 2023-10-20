ef59384aec82305627002519b7db7d0951e12873
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-20 16:36:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def approve(_, m : Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @illuminatiXNetwork__**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id,img, "**Hᴇʟʟᴏ {}!\nWᴇʟᴄᴏᴍᴇ Tᴏ {}\n\n__Pᴏᴡᴇʀᴅ Bʏ : @illuminatiXNetwork__**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
@@ -55,7 +55,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://te.legra.ph/file/0e5e9c92332ec57a06065.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @illuminatiXNetwork__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+            await m.reply_photo("https://graph.org/file/5ef5753c1200a0f29cf3e.jpg", caption="**🦊 Hello {}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @illuminatiXNetwork__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
